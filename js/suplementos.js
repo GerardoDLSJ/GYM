624597cae952d79d9f1bf924752982d992ee60ac
CesarAM12 <100311997+CesarAM12@users.noreply.github.com>
2023-06-01 16:37:00 -0600

    vistas

--- js/suplementos.js
+++ js/suplementos.js
@@ -28,20 +28,35 @@ function templateSuplementos(suplementos) {
   const $template = document.querySelector(".grid");
 
   suplementos.forEach((suplemento) => {
+   
     var titulo = suplemento.titulo;
     var imagen = suplemento.image;
     var precio = suplemento.precio;
+    var descripcion = suplemento.descripcion;
 
     const $clonItemSuplemento = $template.content.cloneNode(true);
-
     $clonItemSuplemento.querySelector("#titulo").textContent = titulo;
     $clonItemSuplemento.querySelector("#image").src = imagen;
     $clonItemSuplemento.querySelector("#price").textContent = "$" + precio;
+    $clonItemSuplemento.querySelector("article").addEventListener('click', function(){
+      document.getElementById('exampleModalLabel').innerHTML = titulo;
+    document.getElementById('info_modal').innerHTML = `
+    <div>
+      <div class="contenedor_mostrar" >
+        <div class="mostrar_imagen"><img src="${imagen}" style="height: 8rem; display: flex; justify-items: center; align-items: center;" alt="Imagen"></div>
+        <div class="mostrar_descripcion">${descripcion}</div>
+      </div>
+        <div class="mostrar_precio">Precio: $${precio}</div>
+    </div>
+    `
+    const modalDescripcion = new bootstrap.Modal(
+        document.getElementById("modalDescripcion")
+      ); 
+      modalDescripcion.show();
+    });
 
     fragment.appendChild($clonItemSuplemento);
   });
-  console.log(fragment);
-
   const main = document.querySelector(".grid");
 
   main.appendChild(fragment);
@@ -65,3 +80,6 @@ document
     });
     renderSuplementos(searchInput);
   });
+
+
+
